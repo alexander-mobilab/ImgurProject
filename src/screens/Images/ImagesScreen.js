0381eee1af088b1f3a811c88fc0a099b0e464801
alexander <alexander@mobilabsolutions.com>
2019-08-22 16:54:41 +0200

    feature: show all images of an array (album)

--- src/screens/Images/ImagesScreen.js
+++ src/screens/Images/ImagesScreen.js
@@ -1,25 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
   Image,
+  TouchableHighlight,
+  FlatList
 } from 'react-native';
+{/*import { TouchableHighlight } from 'react-native-gesture-handler';*/ }
 
 export default class ImagesScreen extends Component {
   render() {
     const { navigation } = this.props;
     const itemID = navigation.getParam('itemID');
-    
+
     const imageMapper = {
-      '1': [require('./img/github.png'),
-            require('./img/slack.png')],
-      '2': [require('./img/slack.png')]
-  }
+      '1': [{ img: require('./img/github.png') },
+      { img: require('./img/slack.png') }],
+      '2': [{ img: require('./img/mobilab_signature.gif') }]
+    }
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Image source={imageMapper[itemID][0] || require('./img/gmail.png')} style ={{height:100, width: 100}}/>
+
+        {/*<Image source={imageMapper[itemID][0].img || require('./img/gmail.png')}
+            style={{ height: 100, width: 100 }}
+      />*/}
+
+        <FlatList data={imageMapper[itemID]}
+          renderItem={({ item }) =>
+            <Image source={item.img}
+              style={{ height: 100, width: 100 }}
+            />}
+            keyExtractor={(item, index) => index.toString()}
+          
+        />
+
       </View>
     );
-  }  
+  }
 }
